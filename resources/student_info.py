346eee87178710599a3f602c044aedf61d5559e4
Brandon Foushee <brandondfoushee@gmail.com>
2024-04-21 08:28:36 -0700

    update

--- resources/student_info.py
+++ resources/student_info.py
@@ -1,5 +1,5 @@
 # Student ID Number. Should be a 9-digit number. Keep any zeros at the beginning.
-SIS_ID = '012345679'
+SIS_ID = '012345678'
 
 # Student Name
-STUDENT_NAME = 'firstname.lastnamea'
+STUDENT_NAME = 'firstname.lastname'
